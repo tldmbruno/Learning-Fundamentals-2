fd629f309cffbcad78d44df35060d21ec46e8095
Bruno Peres <dev.brunoperes@gmail.com>
2023-05-29 13:26:26 -0300

    Add sound effect (vineboom) and music (violinomass)

--- Assets/Code/Scripts/ShooterBehavior.cs
+++ Assets/Code/Scripts/ShooterBehavior.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class ShooterBehavior : MonoBehaviour
 {
-    [SerializeField] private float _forceMagnitude = 800f;
-    [SerializeField] private float _secondsBetweenShots = .2f;
+    [SerializeField] private float _forceMagnitude = 900f;
+    [SerializeField] private float _secondsBetweenShots = .05f;
 
     private Camera _mainCamera;
     private GameObject _projectile;
+    private AudioSource _audioSource;
+    private AudioClip _shootSound;
     private bool _isAbleToShoot = true;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
         _projectile = Resources.Load<GameObject>("Projectiles/FireProjectile");
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _shootSound = Resources.Load<AudioClip>("SFX/vineboom");
     }
 
     private void Update()
@@ -31,6 +35,8 @@ public class ShooterBehavior : MonoBehaviour
         Rigidbody2D projectileBody = Instantiate(_projectile.GetComponent<Rigidbody2D>(), player.transform);
         projectileBody.transform.position += (Vector3)direction;
         projectileBody.AddForce(direction * _forceMagnitude);
+
+        _audioSource.PlayOneShot(_shootSound);
 
         _isAbleToShoot = false;
         StartCoroutine(StartShotCooldown());
